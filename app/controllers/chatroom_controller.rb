f2431f566b2516b2716c21fadfdafde8cc9fb9f2
advya <50411324+Advya@users.noreply.github.com>
2021-11-13 22:16:57 +0200

    initial commit
    project from udemy course

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,2 +1,9 @@
 class ChatroomController < ApplicationController
+    before_action :require_user
+    
+    def home
+      @message = Message.new
+      @messages = Message.custom_display 
+    end
+    
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Pashka", password: "kakashka")
+User.create(username: "Pos", password: "lotos")
+User.create(username: "Pas", password: "latos")
+User.create(username: "Pak", password: "katos")
+
+Message.create(body: "Yea there is some message", user_id: 1)
+Message.create(body: "Oh Hi user 1", user_id: 2)
+Message.create(body: "You both are morons", user_id: 3)
+Message.create(body: "No u!", user_id: 1)
